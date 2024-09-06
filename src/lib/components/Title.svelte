6c8b30480228c08311ad6585f94378c2731a9df9
Sander <sunundershadow@gmail.com>
2024-09-07 00:18:47 +0300

    Created title component

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,15 @@
+<div class="title"><slot/></div>
+
+<style lang="scss">
+  .title {
+    width: min-content;
+
+    background: linear-gradient(90deg, rgba(#B67DBB, 0), #dfc7e1 15%, #f5ddf8, #dfc7e1 85%, rgba(#B67DBB, 0));
+    text-align: center;
+    padding: .75rem 1em;
+    font-size: 3rem;
+
+    color: #370e4a;
+    font-weight: bold;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
   import '$lib/global.scss'
 
   import Container from "$lib/components/Container.svelte"
+  import Title from "$lib/components/Title.svelte";
 </script>
 
 <Container>
-  somedata
+  <Title>ПОДКАТЫ</Title>
 </Container>
