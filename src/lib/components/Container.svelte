5122dd95653f8d1bce671c8d959c6c91314cbd21
Sander <sunundershadow@gmail.com>
2024-09-07 00:04:08 +0300

    Created container component

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,19 @@
+<div class="container">
+  <slot/>
+</div>
+
+<style lang="scss">
+  @use "$lib/config.scss";
+
+  .container {
+    @media (min-width: config.$screen-size-tablet) {
+      max-width: config.$container-width-tablet;
+    }
+    
+    @media (min-width: config.$screen-size-desktop) {
+      max-width: config.$container-width-desktop;
+    }
+
+    margin: 0 auto;
+  }
+</style>
