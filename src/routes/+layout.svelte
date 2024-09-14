a11fdf4a88360b368c41d8066616695ec5be89ac
Sander <sunundershadow@gmail.com>
2024-09-14 19:57:03 +0300

    Layout extracted from page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import '$lib/styles/global.scss'
+
+  import GithubIcon from "$lib/icons/Github.svelte"
+</script>
+
+<a class="git_icon" href="https://github.com/SunderShadow/Tacker_game_front">
+  <GithubIcon/>
+</a>
+
+<slot/>
+
+<style lang="scss">
+  @use "$lib/config.scss";
+
+  .git_icon {
+    display: flex;
+    width: 40px;
+    margin: .5rem 0 0 .5rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,7 @@
 
   import Container from "$lib/components/Container.svelte"
   import Title from "$lib/components/Title.svelte"
-  import GithubIcon from "$lib/icons/Github.svelte"
 </script>
-
-<a href="https://github.com/SunderShadow/Tacker_game_front" class="git_icon">
-  <GithubIcon/>
-</a>
 
 <Container>
   <div class="title">
@@ -38,12 +33,6 @@
 
 <style lang="scss">
   @use "$lib/config.scss";
-
-  .git_icon {
-    display: flex;
-    width: 40px;
-    margin: .5rem 0 0 .5rem;
-  }
 
   .title {
     width: max-content;
